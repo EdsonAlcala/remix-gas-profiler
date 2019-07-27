0f0def4b9925474f326dbace05acf2d02b5be920
Edson Alcala <edson_alcala@msn.com>
2019-07-27 09:46:01 +0100

    added audit, coverage reporter, badge

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Gas Profiler Remix-Plugin
 
+[![CircleCI](https://circleci.com/gh/EdsonAlcala/remix-gas-profiler.svg?style=svg)](https://circleci.com/gh/EdsonAlcala/remix-gas-profiler)
+
 ### Install
 
 > git clone git@github.com:EdsonAlcala/remix-gas-profiler.git
--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,15 @@ module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
+  "reporters": ["default", "jest-junit"],
+  "testEnvironment": "node",
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "json"
+  ]
 }
+
+
