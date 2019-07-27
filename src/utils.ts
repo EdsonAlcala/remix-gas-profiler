bd2c623452625cb556e69a52b63778da2aa6d0f0
Edson Alcala <edson_alcala@msn.com>
2019-07-27 09:47:47 +0100

    prettier

--- src/utils.ts
+++ src/utils.ts
@@ -26,41 +26,41 @@
 // }
 
 export const buildLineOffsets = (src: string) => {
-    let accu = 0
-    return src.split('\n').map(line => {
-        const ret = accu
-        accu += line.length + 1
-        return ret
-    })
+  let accu = 0
+  return src.split('\n').map(line => {
+    const ret = accu
+    accu += line.length + 1
+    return ret
+  })
 }
 
 interface IMapping {
-    [key: string]: number
+  [key: string]: number
 }
 
 export const parsedLast = (srcmap: string) => {
-    return srcmap
-        .split(';')
-        .map(l => l.split(':'))
-        .map(([s, l, f, j]) => ({ s: s === '' ? undefined : s, l, f, j }))
-        .reduce(
-            ([last, ...list], { s, l, f, j }) => [
-                {
-                    s: parseInt(s || last.s, 10),
-                    l: parseInt(l || last.l, 10),
-                    f: parseInt(f || last.f, 10),
-                    j: j || last.j,
-                },
-                last,
-                ...list,
-            ],
-            [{} as any],
-        )
-        .reverse()
-        .slice(1)
-    // .map(
-    //     ({ s, l, f, j }: any) => `${srcmaps.sourceList[f]}:${getLineFromPos(source, s)}`
-    // );
+  return srcmap
+    .split(';')
+    .map(l => l.split(':'))
+    .map(([s, l, f, j]) => ({ s: s === '' ? undefined : s, l, f, j }))
+    .reduce(
+      ([last, ...list], { s, l, f, j }) => [
+        {
+          s: parseInt(s || last.s, 10),
+          l: parseInt(l || last.l, 10),
+          f: parseInt(f || last.f, 10),
+          j: j || last.j,
+        },
+        last,
+        ...list,
+      ],
+      [{} as any],
+    )
+    .reverse()
+    .slice(1)
+  // .map(
+  //     ({ s, l, f, j }: any) => `${srcmaps.sourceList[f]}:${getLineFromPos(source, s)}`
+  // );
 }
 
 const isPush = (instruction: any) => instruction >= 0x60 && instruction < 0x7f // TODO: Improve types
@@ -68,108 +68,108 @@ const isPush = (instruction: any) => instruction >= 0x60 && instruction < 0x7f /
 const pushDataLength = (instruction: any) => instruction - 0x5f
 
 const instructionLength = (instruction: any) =>
-    isPush(instruction) ? 1 + pushDataLength(instruction) : 1
+  isPush(instruction) ? 1 + pushDataLength(instruction) : 1
 
 const byteToInstIndex = (binary: string) => {
-    const result = []
-    let byteIndex = 0
-    let instIndex = 0
-    while (byteIndex < binary.length) {
-        const length = instructionLength(binary[byteIndex])
-        for (let i = 0; i < length; i += 1) {
-            result.push(instIndex)
-        }
-        byteIndex += length
-        instIndex += 1
+  const result = []
+  let byteIndex = 0
+  let instIndex = 0
+  while (byteIndex < binary.length) {
+    const length = instructionLength(binary[byteIndex])
+    for (let i = 0; i < length; i += 1) {
+      result.push(instIndex)
     }
-    return result
+    byteIndex += length
+    instIndex += 1
+  }
+  return result
 }
 
 export const parseRuntimeBinary = (binary: string) => {
-    return byteToInstIndex(binary)
+  return byteToInstIndex(binary)
 }
 
 export const buildPcToInstructionMapping = (code0xHexStr: string) => {
-    const mapping: IMapping = {}
-    const codeHexStr = code0xHexStr.slice(2)
-    let instructionIndex = 0
-    console.log('codeHexStr.length', codeHexStr.length)
-    console.log('codeHexStr', codeHexStr)
+  const mapping: IMapping = {}
+  const codeHexStr = code0xHexStr.slice(2)
+  let instructionIndex = 0
+  console.log('codeHexStr.length', codeHexStr.length)
+  console.log('codeHexStr', codeHexStr)
 
-    for (let pc = 0; pc < codeHexStr.length / 2;) {
-        console.log('PC Counter', pc)
-        mapping[pc] = instructionIndex
+  for (let pc = 0; pc < codeHexStr.length / 2; ) {
+    console.log('PC Counter', pc)
+    mapping[pc] = instructionIndex
 
-        const byteHex = codeHexStr[pc * 2] + codeHexStr[pc * 2 + 1]
-        const byte = parseInt(byteHex, 16)
+    const byteHex = codeHexStr[pc * 2] + codeHexStr[pc * 2 + 1]
+    const byte = parseInt(byteHex, 16)
 
-        // PUSH instruction has immediates
-        if (byte >= 0x60 && byte <= 0x7f) {
-            const n = byte - 0x60 + 1 // number of immediates
-            pc += n + 1
-        } else {
-            pc += 1
-        }
-        instructionIndex += 1
+    // PUSH instruction has immediates
+    if (byte >= 0x60 && byte <= 0x7f) {
+      const n = byte - 0x60 + 1 // number of immediates
+      pc += n + 1
+    } else {
+      pc += 1
     }
-    return mapping
+    instructionIndex += 1
+  }
+  return mapping
 }
 
 export const parseAnotherSourceMap = (sourceMap: string) => {
-    const items = sourceMap.trim().split(';')
+  const items = sourceMap.trim().split(';')
 }
 
 const scan = (array: [], reducer: any, initialValue: []) => {
-    let accumulator = initialValue
-    const result = []
+  let accumulator = initialValue
+  const result = []
 
-    for (const currentValue of array) {
-        const curr = reducer(accumulator, currentValue)
-        accumulator = curr
-        result.push(curr)
-    }
-    return result
+  for (const currentValue of array) {
+    const curr = reducer(accumulator, currentValue)
+    accumulator = curr
+    result.push(curr)
+  }
+  return result
 }
 
 // https://solidity.readthedocs.io/en/develop/miscellaneous.html#source-mappings
 export const parseSourceMap = (sourceMap: string) => {
-    let prevS: string
-    let prevL: string
-    let prevF: string
-    let prevJ: string
+  let prevS: string
+  let prevL: string
+  let prevF: string
+  let prevJ: string
 
-    console.log('SourceMapToParse', sourceMap)
+  console.log('SourceMapToParse', sourceMap)
 
-    return sourceMap
-        .trim()
-        .split(';')
-        .map(section => {
-            let [s, l, f, j] = section.split(':')
+  return sourceMap
+    .trim()
+    .split(';')
+    .map(section => {
+      let [s, l, f, j] = section.split(':')
 
-            if (s === '' || s === undefined) {
-                s = prevS
-            } else {
-                prevS = s
-            }
+      if (s === '' || s === undefined) {
+        s = prevS
+      } else {
+        prevS = s
+      }
 
-            if (l === '' || l === undefined) {
-                l = prevL
-            } else {
-                prevL = l
-            }
+      if (l === '' || l === undefined) {
+        l = prevL
+      } else {
+        prevL = l
+      }
 
-            if (f === '' || f === undefined) {
-                f = prevF
-            } else {
-                prevF = f
-            }
+      if (f === '' || f === undefined) {
+        f = prevF
+      } else {
+        prevF = f
+      }
 
-            if (j === '' || j === undefined) {
-                j = prevJ
-            } else {
-                prevJ = j
-            }
+      if (j === '' || j === undefined) {
+        j = prevJ
+      } else {
+        prevJ = j
+      }
 
-            return { s: Number(s), l: Number(l), f: Number(f), j }
-        })
+      return { s: Number(s), l: Number(l), f: Number(f), j }
+    })
 }
