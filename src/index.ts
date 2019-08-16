42abe3424d62d2fcf860c5e2423bafd96cdf0828
Edson Alcala <edson_alcala@msn.com>
2019-08-16 08:36:01 +0100

    added renderer

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {
   RemixTx,
 } from '@remixproject/plugin'
 import { Profiler } from './profiler'
+import { getCostsColumn, getCodeWithGasCosts, transactionHeader } from './renderer';
 
 const devMode = { port: 8080 }
 
@@ -46,7 +47,7 @@ export class GasProfilerPlugin {
         const contractSourceKeys = Object.keys(contracts)
         const contractSourceKey = contractSourceKeys[0]
 
-        const originalSourceCode = (compilationResult as any).source.sources[contractSourceKey].content;
+        const originalSourceCode = (compilationResult as any).source.sources[contractSourceKey].content.trim();
         console.log('originalSourceCode', originalSourceCode)
 
         const contractKeys = Object.keys(contracts[contractSourceKey])
@@ -86,45 +87,8 @@ export class GasProfilerPlugin {
 
   private render(originalSourceCode, gasPerLineCost, transaction) {
 
-    const getCostsColumn = (gasPerLineCost) => {
-      let result = ''
-      gasPerLineCost.forEach(item => {
-        const currentCell =
-          item.gasCost > 0
-            ? `<span class='gas-amount'>${item.gasCost}</span>`
-            : `<span class='empty'>---</span>`
-        result += currentCell
-      })
-      return result
-    }
-
-    const getCodeWithGasCosts = (costsColumn, originalSourceCode) => (`
-      <table><tbody>
-        <tr>
-          <td class="gas-costs">
-            ${costsColumn}
-          </td>
-          <td><pre class="prettyprint lang-js">${originalSourceCode}</pre></td>
-        </tr>
-      </tbody></table>
-      `
-    )
-
     const costsColumn = getCostsColumn(gasPerLineCost)
     const codeWithGasCosts = getCodeWithGasCosts(costsColumn, originalSourceCode)
-
-    const transactionHeader = ({ hash, transactionCost, executionCost, contractAddress }) => `
-        <li class="custom list-group-item d-print-flex">
-          <h6>Transaction hash</h6>
-          <p class="badge badge-light">${hash}</p>
-          <h6> Contract address </h6>
-          <p class="badge badge-light">${contractAddress}</p>
-          <h6>Transaction cost</h6>
-          <p class="badge badge-light">${transactionCost}</p>
-          <h6>Execution cost</h6>
-          <p class="badge badge-light">${executionCost}</p>
-        </li>
-    `
 
     const htmlContent = `
       <ul class="list-group">
---
+++ src/renderer.ts
@@ -0,0 +1,38 @@
+export const getCostsColumn = (gasPerLineCost) => {
+    let result = ''
+    gasPerLineCost.forEach(item => {
+        const currentCell =
+            item.gasCost > 0
+                ? `<span class='gas-amount'>${item.gasCost}</span>`
+                : `<span class='empty'>0</span>`
+        result += currentCell
+    })
+    return result
+}
+
+export const getCodeWithGasCosts = (costsColumn, originalSourceCode) => (`
+<table><tbody>
+  <tr>
+    <td class="gas-costs">
+      ${costsColumn}
+    </td>
+    <td>
+        <pre class="prettyprint lang-js">${originalSourceCode}</pre>
+    </td>
+  </tr>
+</tbody></table>
+`
+)
+
+export const transactionHeader = ({ hash, transactionCost, executionCost, contractAddress }) => `
+        <li class="custom list-group-item d-print-flex">
+          <h6>Transaction hash</h6>
+          <p class="badge badge-light">${hash}</p>
+          <h6> Contract address </h6>
+          <p class="badge badge-light">${contractAddress}</p>
+          <h6>Transaction cost</h6>
+          <p class="badge badge-light">${transactionCost}</p>
+          <h6>Execution cost</h6>
+          <p class="badge badge-light">${executionCost}</p>
+        </li>
+    `
